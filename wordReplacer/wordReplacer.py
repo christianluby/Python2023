6e15adbc60ffcab1287bdc344d3ea33fbea6233b
christianluby <110574868+christianluby@users.noreply.github.com>
2023-08-03 00:55:16 -0400

    Add files via upload

---
+++ wordReplacer/wordReplacer.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "Yo yo yo what is going on guys it is your boy optic luby"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word to replacement: ")
+    print(str.replace(word_to_replace,word_replacement))
+
+replace_word()
