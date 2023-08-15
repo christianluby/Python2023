13aa358b1432c457715b693419abce24b536dfb9
christianluby <110574868+christianluby@users.noreply.github.com>
2023-08-15 17:53:07 -0400

    Add files via upload

---
+++ diceRoller.py
@@ -0,0 +1,18 @@
+import random
+
+
+def roll_dice():
+
+    
+
+    roll = input("Roll dice? (Yes/No): ")
+
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+
+        print("dice rolled: {} and {}".format(dice1, dice2))
+
+        roll = input("Roll Again? (Yes/No?): ")
+
+roll_dice()
